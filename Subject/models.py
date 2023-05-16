b42522fa1de44c60a9fee6c16106d144cfcd9d35
BeshoiBotros <beshoibotros111@gmail.com>
2023-05-16 20:18:08 +0300

    s

--- Subject/models.py
+++ Subject/models.py
@@ -2,22 +2,22 @@ from django.db import models
 from Doctor.models import Doctor
 
 class Subject(models.Model):
-    name = models.TextField(max_length=255)
+    name = models.CharField(max_length=255)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, null=False)
     price = models.FloatField(max_length=5, default=0.0)
     pdf = models.FileField(upload_to='subjects/pdfs/', null=True)
 
 class Chapter(models.Model):
-    title = models.TextField(max_length=255)
+    title = models.CharField(max_length=255)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=False)
 
 class Video(models.Model):
-    title = models.TextField(max_length=255)
+    title = models.CharField(max_length=255)
     video = models.FileField(upload_to='subjects/videos/', null=False)
     chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE, null=False)
 
 class TrueOrFalseQ(models.Model):
-    quistion = models.TextField(max_length=255)
+    quistion = models.CharField(max_length=255)
     answer   = models.BooleanField(null=False)
 
 class MultipleChoiceQ(models.Model):
@@ -29,12 +29,12 @@ class MultipleChoiceQ(models.Model):
         ('d', 'd')
     ]
 
-    quistion = models.TextField(max_length=255)
-    choiceA  = models.TextField(max_length=255)
-    choiceB  = models.TextField(max_length=255)
-    choiceC  = models.TextField(max_length=255)
-    choiceD  = models.TextField(max_length=255)
-    answer   = models.TextField(choices=answer_choices, default='a')
+    quistion = models.CharField(max_length=255)
+    choiceA  = models.CharField(max_length=255)
+    choiceB  = models.CharField(max_length=255)
+    choiceC  = models.CharField(max_length=255)
+    choiceD  = models.CharField(max_length=255)
+    answer   = models.CharField(choices=answer_choices, default='a')
     degree   = models.FloatField(null=False, default=0.0, max_length=5)
 
 class AsSayQ(models.Model):
