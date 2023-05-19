b8701a94e25832b98359992c15cfb436a97b1255
BeshoiBotros <beshoibotros111@gmail.com>
2023-05-19 02:19:32 -0700

    edited by beshoy

--- Organization/views.py
+++ Organization/views.py
@@ -6,6 +6,7 @@ from .models import Organization
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework import status
 
 @api_view(['POST'])
 @permission_classes([OrganizationPermission])
@@ -17,15 +18,15 @@ def createDoctorView(request):
         user = serializer.create(request.data)
     except:
         msg = "User already exist try again"
-        return Response({'msg' : msg})
+        return Response({'msg' : msg}, status=status.HTTP_400_BAD_REQUEST)
     try:
         org = Organization.objects.get(user = request.user)
     except:
         msg = "Authentication credentials were not provided."
-        return Response({'msg' : msg})
+        return Response({'msg' : msg}, status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
     try:
         doctor = Doctor.objects.create(user = User.objects.get(username = user.username), organization=org)
     except:
         msg = "something goes wrong!"
     msg = "Doctor created successfuly"
-    return Response({'msg' : msg})
+    return Response({'msg' : msg}, status=status.HTTP_201_CREATED)
