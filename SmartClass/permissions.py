e7b694606df104213b2f34247c568eca5ecb4763
BeshoiBotros <beshoibotros111@gmail.com>
2023-06-20 21:12:16 +0300

    "add doctor permissions"

--- SmartClass/permissions.py
+++ SmartClass/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework.permissions import BasePermission
-from Organization.models import Organization
+from Organization.models import Organization, Doctor
 
 class OrganizationPermission(BasePermission):
 
@@ -22,4 +22,17 @@ class OrganizationPermission(BasePermission):
         
 
 class DoctorPermission(BasePermission):
-    pass
+    def has_object_permission(self, request, view, obj):
+        try:
+            doc = Doctor.objects.get(user = request.user.id)
+            if obj.user == doc.user:
+                return True
+            return False
+        except Doctor.DoesNotExist:
+            return False
+    def has_permission(self, request, view):
+        try:
+            doc = Doctor.objects.get(user = request.user.id)
+            return True
+        except Doctor.DoesNotExist:
+            return False
